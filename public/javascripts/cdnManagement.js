c5baef28da3df5e6306080803d7d667b97ecebba
xpecham@stuba.sk <chapes1000>
2017-01-16 21:23:14 +0100

    required APIs working

--- public/javascripts/cdnManagement.js
+++ public/javascripts/cdnManagement.js
@@ -16,6 +16,7 @@ app.controller('TabController', ['$cookieStore', '$scope', '$http', '$window', f
 
     $scope.formCreateContent = {};
     $scope.formCreateDelSer = {};
+    $scope.formAssignDelSer = {};
 
     var deliveryServicesReceived = 0;
     var serviceEnginesReceived = 0;
@@ -243,13 +244,19 @@ app.controller('TabController', ['$cookieStore', '$scope', '$http', '$window', f
     //---------------------------------------------------------------------------------------------------------------------------------------------------
     //---------------------------------------------------------------------------------------------------------------------------------------------------
 
-    //GET ALL CONTENT ORIGINS
 
+    // call function to merge 2 requests , get delivery services and get service engines
+    // loop through responses and find service engines for delivery services
     $scope.addSeName = function () {
         var arrDeliveryServices = [];
         for (var i = 0, length = $scope.deliveryServicesData.length; i < length; i++) {
-            for (var j = 0, length = $scope.serviceEnginesData.length; j < length; j++) {
+            var foundSe = 0;
+            for (var j = 0, length2 = $scope.serviceEnginesData.length; j < length2; j++) {
+
+                console.log(j + 1);
+                console.log($scope.serviceEnginesData.length);
                 if ($scope.deliveryServicesData[i].seID === $scope.serviceEnginesData[j].id) {
+                    foundSe = 1;
                     var deliveryService = {
                         name: $scope.deliveryServicesData[i].name,
                         originFqdn: $scope.deliveryServicesData[i].originFqdn,
@@ -261,6 +268,22 @@ app.controller('TabController', ['$cookieStore', '$scope', '$http', '$window', f
                     }
                     console.log(deliveryService);
                     arrDeliveryServices.push(deliveryService);
+                    break;
+                }
+
+                if ((j + 1 === $scope.serviceEnginesData.length)) {
+
+                    var deliveryService = {
+                        name: $scope.deliveryServicesData[i].name,
+                        originFqdn: $scope.deliveryServicesData[i].originFqdn,
+                        rfqdn: $scope.deliveryServicesData[i].rfqdn,
+                        id: $scope.deliveryServicesData[i].id,
+                        originID: $scope.deliveryServicesData[i].originID,
+                        seID: null,
+                        seName: "Unassigned"
+                    }
+                    console.log(deliveryService);
+                    arrDeliveryServices.push(deliveryService);
                 }
             }
         }
@@ -269,6 +292,7 @@ app.controller('TabController', ['$cookieStore', '$scope', '$http', '$window', f
         deliveryServicesReceived = 0;
     };
 
+    // GET ALL DELIVERY SERVICES
     $http.get('/getDeliveryServices')
         .success(function (data) {
             var arrContentOrigins = [];
@@ -290,7 +314,9 @@ app.controller('TabController', ['$cookieStore', '$scope', '$http', '$window', f
             }
             $scope.deliveryServicesData = arrContentOrigins;
             var arrDeliveryServices = [];
+            //this is check if response come first from get Service engines or get delivery services
             deliveryServicesReceived = 1;
+            //if service engines was received then call addSeName FUNCTIONS
             if (serviceEnginesReceived == 1) {
                 $scope.addSeName();
             }
@@ -315,9 +341,10 @@ app.controller('TabController', ['$cookieStore', '$scope', '$http', '$window', f
 
             }
             $scope.serviceEnginesData = arrContentOrigins;
-
+            //this is check if response come first from get Service engines or get delivery services
             serviceEnginesReceived = 1;
             var arrDeliveryServices = [];
+            //if delivery services was received then call function
             if (deliveryServicesReceived == 1) {
                 $scope.addSeName();
             }
@@ -329,9 +356,24 @@ app.controller('TabController', ['$cookieStore', '$scope', '$http', '$window', f
 
     //CREATE DELIVERY SERVICE
     $scope.createDeliveryService = function () {
-        console.log($scope.formCreateDelSer);
-        
-        $http.post('/createContentOrigin', $scope.formCreateContent)
+        waitingDialog.show();
+        var origName = $scope.formCreateDelSer.contentOrigin;
+        var originID = 0;
+        // loop through contentOriginsData because in front end is showed only name, so we must find ID for name
+        for (var i = 0, len = $scope.contentOriginsData.length; i < len; i++) {
+            if ($scope.contentOriginsData[i].name === origName) {
+                originID = $scope.contentOriginsData[i].id;
+                break;
+            }
+        }
+
+        var tempObj = {
+            serName: $scope.formCreateDelSer.serName,
+            idOrigin: originID
+        }
+
+        console.log(tempObj)
+        $http.post('/createDeliveryService', tempObj)
             .success(function (data) {
                 $scope.formData = {};
                 $scope.cdnData = data;
@@ -342,15 +384,87 @@ app.controller('TabController', ['$cookieStore', '$scope', '$http', '$window', f
             });
     };
 
-    $scope.editingData = {};
+    //DELETE DELIVERY SERVICE
+    $scope.deleteDeliveryService = function (ID) {
+        waitingDialog.show();
+        $http.delete('/deleteDeliveryService/' + ID)
+            .success(function (data) {
+                $scope.formData = {};
+                $scope.cdnData = data;
+                $window.location.reload();
+            })
+            .error(function (error) {
+                console.log('Error: ' + error);
+            });
+    };
 
-    for (var i = 0, length = $scope.contentOriginsData.length; i < length; i++) {
-        $scope.editingData[$scope.contentOriginsData[i].id] = false;
+    //ASSIGN/UNASSIGN SERVICE ENGINE
+    $scope.assignSE = function (delSer, swtch) {
+        waitingDialog.show();
+        // because in table there are 2 sources of data, from deliveryServicesAll and in dropdown serviceEnginesData, delivery service is passed through function and seName through scope
+        var seName = $scope.formAssignDelSer.seName;
+        var delSerID;
+        var seID;
+        //find IDs according to name 
+        for (var i = 0, len = $scope.deliveryServicesAll.length; i < len; i++) {
+            if ($scope.deliveryServicesAll[i].name === delSer) {
+                delSerID = $scope.deliveryServicesAll[i].id;
+                break;
+            }
+        }
+
+        for (var i = 0, len = $scope.serviceEnginesData.length; i < len; i++) {
+            if ($scope.serviceEnginesData[i].name === seName) {
+                seID = $scope.serviceEnginesData[i].id;
+                break;
+            }
+        }
+
+        var tempObj = {
+            delSerID: delSerID,
+            seID: seID
+        }
+
+        console.log(delSerID + "   " + seID);
+        if (swtch === 1) {
+            $http.post('/assignSE', tempObj)
+                .success(function (data) {
+                    $scope.formData = {};
+                    $scope.cdnData = data;
+                    $window.location.reload();
+                })
+                .error(function (error) {
+                    console.log('Error: ' + error);
+                });
+        }
+        else{
+            $http.post('/unAssignSE', tempObj)
+                .success(function (data) {
+                    $scope.formData = {};
+                    $scope.cdnData = data;
+                    $window.location.reload();
+                })
+                .error(function (error) {
+                    console.log('Error: ' + error);
+                });
+        }
+
+    };
+
+
+    $scope.editingDataDelivery = {};
+
+    for (var i = 0, length = $scope.deliveryServicesAll.length; i < length; i++) {
+        $scope.editingDataDelivery[$scope.deliveryServicesAll[i].id] = false;
     }
 
-    $scope.modify = function (tableData) {
-        $scope.editingData[tableData.id] = true;
+    $scope.modifyDelivery = function (tableData) {
+        $scope.editingDataDelivery[tableData.id] = true;
     };
+
+    $scope.cancelDelivery = function (tableData) {
+        $scope.editingDataDelivery[tableData.id] = false
+    }
 
 
     /*
@@ -374,7 +488,7 @@ app.controller('TabController', ['$cookieStore', '$scope', '$http', '$window', f
                 console.log('Error: ' + error);
             });
     };
-
+ 
     //DELETE USER
     $scope.deleteUser = function() {
         $http.delete('/api/v1/users/' + userID)
--- routes/index.js
+++ routes/index.js
@@ -566,7 +566,7 @@ router.get('/getDeliveryServices', function (req, res) {
     );
 });
 
-//GET SE SERVICES
+//GET SERVICE ENGINES
 router.get('/getSE', function (req, res) {
 
     var request = require('request');
@@ -605,6 +605,147 @@ router.get('/getSE', function (req, res) {
         }
     );
 });
+
+//CREATE DELIVERY SERVICE
+router.post('/createDeliveryService', function (req, res) {
+
+    var request = require('request');
+    var request = request.defaults({
+        strictSSL: false,
+        rejectUnauthorized: false
+    });
+
+    username = "admin",
+        password = "CdnLab_123",
+        url = "https://cdsm.cdn.ab.sk:8443/servlet/com.cisco.unicorn.ui.ChannelApiServlet?action=createDeliveryService&deliveryService=" + req.body.serName + "&contentOrigin=" + req.body.idOrigin,
+        console.log(url);
+    auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+
+    request(
+        {
+            url: url,
+            headers: {
+                "Authorization": auth
+            }
+        },
+        function (error, response, body) {
+            if (error != null || body != null) {
+                //console.log(body);
+            }
+            if (response != null) {
+                return res.json(response);
+            }
+        }
+    );
+});
+
+//DELETE DELIVERY SERVICE
+router.delete('/deleteDeliveryService/:ID', function (req, res) {
+
+    var results = [];
+
+    // Grab data from the URL parameters
+    var id = req.params.ID;
+
+    var request = require('request');
+    var request = request.defaults({
+        strictSSL: false,
+        rejectUnauthorized: false
+    });
+
+    username = "admin",
+        password = "CdnLab_123",
+        url = "https://cdsm.cdn.ab.sk:8443/servlet/com.cisco.unicorn.ui.ChannelApiServlet?action=deleteDeliveryServices&deliveryService=" + id,
+        console.log(url);
+    auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+
+    request(
+        {
+            url: url,
+            headers: {
+                "Authorization": auth
+            }
+        },
+        function (error, response, body) {
+            if (error != null || body != null) {
+                console.log(body);
+            }
+            if (response != null) {
+                return res.json(response);
+            }
+        }
+    );
+
+});
+
+//ASSIGN SERVICE ENGINE
+router.post('/assignSE', function (req, res) {
+
+    var request = require('request');
+    var request = request.defaults({
+        strictSSL: false,
+        rejectUnauthorized: false
+    });
+
+    username = "admin",
+        password = "CdnLab_123",
+        url = "https://cdsm.cdn.ab.sk:8443/servlet/com.cisco.unicorn.ui.ChannelApiServlet?action=assignSEs&deliveryService=" + req.body.delSerID + "&contentAcquirer=" + req.body.seID + "&se=" + req.body.seID,
+        console.log(url);
+    auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+
+    request(
+        {
+            url: url,
+            headers: {
+                "Authorization": auth
+            }
+        },
+        function (error, response, body) {
+            if (error != null || body != null) {
+                //console.log(body);
+            }
+            if (response != null) {
+                return res.json(response);
+            }
+        }
+    );
+});
+
+//UNASSIGN SERVICE ENGINE
+router.post('/unAssignSE', function (req, res) {
+
+    var request = require('request');
+    var request = request.defaults({
+        strictSSL: false,
+        rejectUnauthorized: false
+    });
+
+    username = "admin",
+        password = "CdnLab_123",
+        url = "https://cdsm.cdn.ab.sk:8443/servlet/com.cisco.unicorn.ui.ChannelApiServlet?action=unassignSEs&deliveryService=" + req.body.delSerID + "&se=all",
+        console.log(url);
+    auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+
+    request(
+        {
+            url: url,
+            headers: {
+                "Authorization": auth
+            }
+        },
+        function (error, response, body) {
+            if (error != null || body != null) {
+                //console.log(body);
+            }
+            if (response != null) {
+                return res.json(response);
+            }
+        }
+    );
+});
+
+
+
 
 
 
