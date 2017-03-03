01863760508edc23f35a5079e2d74eb782e2e882
Martin Pecha <pemattt@gmail.com>
2017-03-03 16:19:33 +0100

    set lists almost finished

--- routes/backupCdsmAPI.js
+++ routes/backupCdsmAPI.js
@@ -1,13 +1,8 @@
 var setContentOrigins = function (data, cdsmUrl, rfqdn) {
 
-    
     //because for can be called only one time there is this hack used
-    var j = -1;
-
     //loop through all content origins of other side
     for (var i = 0, len = data.listing.record.length; i < len; i++) {
-
-        j++;
 
         var obj = data.listing.record[i];
 
@@ -18,55 +13,124 @@ var setContentOrigins = function (data, cdsmUrl, rfqdn) {
             id: obj.$.Id
         };
 
-        if (rfqdn.length === 0){
+        if (rfqdn.length === 0) {
             //return no rfqdn available 
         }
 
-        for (j; j < rfqdn.length; j++) {
 
-            var request = require('request');
-            var request = request.defaults({
-                strictSSL: false,
-                rejectUnauthorized: false
-            });
+        var request = require('request');
+        var request = request.defaults({
+            strictSSL: false,
+            rejectUnauthorized: false
+        });
 
-            username = "admin",
-                password = "CdnLab_123",
-                url = cdsmUrl + ":8443/servlet/com.cisco.unicorn.ui.ChannelApiServlet?action=createContentOrigin&name=" + conOrig.name + "&origin=" + conOrig.originFqdn + "&fqdn=" + rfqdn[j],
-                //console.log(url);
-                auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+        username = "admin",
+            password = "CdnLab_123",
+            url = cdsmUrl + ":8443/servlet/com.cisco.unicorn.ui.ChannelApiServlet?action=createContentOrigin&name=" + conOrig.name + "&origin=" + conOrig.originFqdn + "&fqdn=" + rfqdn[i],
+            //console.log(url);
+            auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
 
-            request(
-                {
-                    url: url,
-                    headers: {
-                        "Authorization": auth
-                    }
-                },
-                function (error, response, body) {
-                    if (response != null) {
-                        var parseString = require('xml2js').parseString;
-                        parseString(response.body, function (err, result) {
-                            if (result.deliveryserviceProvisioning.message[0].$.status === "fail") {
-
-                            }
-                            else {
-                                var id = result.deliveryserviceProvisioning.record[0].$.Id;
-                                console.log(id);
-
-                                
-                                
-
-                            }
-                        });
-
-                    }
+        request(
+            {
+                url: url,
+                headers: {
+                    "Authorization": auth
                 }
-            );
-            break;
-        //end for
-        }
+            },
+            function (error, response, body) {
+                if (response != null) {
+                    var parseString = require('xml2js').parseString;
+                    parseString(response.body, function (err, result) {
+                        if (result.deliveryserviceProvisioning.message[0].$.status === "fail") {
 
+                        }
+                        else {
+                            var id = result.deliveryserviceProvisioning.record[0].$.Id;
+                            var name = result.deliveryserviceProvisioning.record[0].$.Name;
+
+                            console.log(id);
+
+                            username = "admin",
+                                password = "CdnLab_123",
+                                url = cdsmUrl + ":8443/servlet/com.cisco.unicorn.ui.ChannelApiServlet?action=createDeliveryService&deliveryService=" + name + "&contentOrigin=" + id,
+                                //console.log(url);
+                                auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+
+                            request(
+                                {
+                                    url: url,
+                                    headers: {
+                                        "Authorization": auth
+                                    }
+                                },
+                                function (error, response, body) {
+                                    if (response != null) {
+                                        var parseString = require('xml2js').parseString;
+
+                                        parseString(response.body, function (err, result) {
+                                            if (result.deliveryserviceProvisioning.message[0].$.status === "fail") {
+
+                                            }
+                                            else {
+                                                var id = result.deliveryserviceProvisioning.record[0].$.Id;
+                                                console.log(id);
+
+                                                username = "admin",
+                                                    password = "CdnLab_123",
+                                                    url = cdsmUrl + ":8443/servlet/com.cisco.unicorn.ui.DeviceApiServlet?action=getDevices&type=SE",
+                                                    auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+
+                                                request(
+                                                    {
+                                                        url: url,
+                                                        headers: {
+                                                            "Authorization": auth
+                                                        }
+                                                    },
+                                                    function (error, response, body) {
+
+                                                        if (response != null) {
+                                                            var devices = [];
+                                                            var parseString = require('xml2js').parseString;
+                                                            parseString(response.body, function (err, result) {
+                                                                if (result.listing.message[0].$.status === "fail") {
+
+                                                                }
+                                                                else {
+                                                                    for (var i = 0, len = result.listing.device.length; i < len; i++) {
+                                                                        if (result.listing.device[i].$.status != "Offline") {
+                                                                            devices.push(result.listing.device[i]);
+                                                                        }
+                                                                    }
+                                                                    console.log(devices);
+
+                                                                    for (var i = 0; i < devices.length ; i++){
+
+                                                                    }
+
+                                                                }
+
+
+                                                            });
+                                                        }
+                                                    }
+                                                );
+
+                                            }
+                                        });
+                                    }
+
+                                }
+                            );
+
+
+                        }
+                    });
+
+                }
+            }
+        );
+        //end for
     }
 }
 
