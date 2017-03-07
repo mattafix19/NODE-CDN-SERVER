2c26300d2abed0cbd167ae0a32bf3618dd64069f
didinozka <drabik.d@gmail.com>
2017-03-07 16:44:27 +0100

    gfdgf

---
+++ models/offer.js
@@ -0,0 +1,18 @@
+/**
+ * Created by duski on 01/03/2017.
+ */
+
+function Offer() {
+    // TODO: Created status ciselko dat, alebo daco co davas pri vytvoreni
+    this.status = 5;
+
+    this.registerOffer = function (toEndpoint) {
+
+    };
+
+    this.notifyOffer = function (toEndpoint) {
+        
+    }
+}
+
+module.exports = Offer;
